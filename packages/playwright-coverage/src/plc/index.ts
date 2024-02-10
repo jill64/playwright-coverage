7a2c158bf6eb6f4219ec3190259f9c5874d415a3
jill64 <intents.turrets0h@icloud.com>
2024-02-10 23:33:27 +0900

    add cli options

--- packages/playwright-coverage/src/plc/index.ts
+++ packages/playwright-coverage/src/plc/index.ts
@@ -7,9 +7,34 @@ import { preprocess } from './preprocess.js'
 export const plc = new App(
   {
     options: {
+      serve: {
+        alias: 's',
+        description: 'Path to source root',
+        type: 'string'
+      },
       output: {
         alias: 'o',
-        description: 'Output coverage report',
+        description: 'Output directory',
+        type: 'string'
+      },
+      help: {
+        alias: 'h',
+        description: 'Show help',
+        type: 'string'
+      },
+      version: {
+        alias: 'v',
+        description: 'Show version',
+        type: 'string'
+      },
+      quiet: {
+        alias: 'q',
+        description: 'Suppress logging',
+        type: 'string'
+      },
+      debug: {
+        alias: 'd',
+        description: 'Debug mode',
         type: 'string'
       }
     },
